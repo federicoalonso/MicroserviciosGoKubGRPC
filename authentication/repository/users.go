612302b476cf975af12c34edad547ad815684c47
Federico Alonso <f.nicolas.alonso@gmail.com>
2021-07-12 20:29:02 -0300

    grpc

---
+++ authentication/repository/users.go
@@ -0,0 +1,21 @@
+package repository
+
+import (
+	"microservicios/db"
+
+	"gopkg.in/mgo.v2"
+)
+
+const UsersCollection = "users"
+
+type UsersRepository interface{}
+
+type usersRepository struct {
+	c *mgo.Collection
+}
+
+func NewUsersRepository(conn db.Connection) UsersRepository {
+	return &usersRepository{c: conn.DB().C(UsersCollection)}
+}
+
+func (r *usersRepository) Save() {}
--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module microservicios
 go 1.16
 
 require (
+	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/joho/godotenv v1.3.0
+	google.golang.org/protobuf v1.27.1 // indirect
 	gopkg.in/mgo.v2 v2.0.0-20190816093944-a6b53ec6cb22
 )
