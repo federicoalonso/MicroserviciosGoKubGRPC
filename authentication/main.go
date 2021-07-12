61d1789906eb2366064914ee486bd0f998292b7b
Federico Alonso <f.nicolas.alonso@gmail.com>
2021-07-12 19:59:29 -0300

    coneccion a base de datos

---
+++ authentication/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"microservicios/db"
+
+	"github.com/joho/godotenv"
+)
+
+var (
+	local bool
+)
+
+func init() {
+	flag.BoolVar(&local, "local", true, "run service local")
+	flag.Parse()
+}
+
+func main() {
+	if local {
+		err := godotenv.Load()
+		if err != nil {
+			log.Panic(err)
+		}
+	}
+
+	cfg := db.NewConfig()
+	conn, err := db.NewConnection(cfg)
+	if err != nil {
+		log.Panic(err)
+	}
+	defer conn.Close()
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module microservicios
+
+go 1.16
+
+require (
+	github.com/joho/godotenv v1.3.0
+	gopkg.in/mgo.v2 v2.0.0-20190816093944-a6b53ec6cb22
+)
