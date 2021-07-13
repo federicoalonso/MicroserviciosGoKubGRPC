06f9912da5e8dd9530fd7c2e89ea6c631400b6c2
Federico Alonso <f.nicolas.alonso@gmail.com>
2021-07-12 21:36:34 -0300

    user save test

---
+++ authentication/repository/users_test.go
@@ -0,0 +1,50 @@
+package repository
+
+import (
+	"fmt"
+	"log"
+	"microservicios/authentication/models"
+	"microservicios/db"
+	"testing"
+	"time"
+
+	"github.com/joho/godotenv"
+	"github.com/stretchr/testify/assert"
+	"gopkg.in/mgo.v2/bson"
+)
+
+func init() {
+	err := godotenv.Load("../.env")
+	if err != nil {
+		log.Panicln(err)
+	}
+}
+
+func TestUsersRepositorySave(t *testing.T) {
+	cfg := db.NewConfig()
+	conn, err := db.NewConnection(cfg)
+	assert.NoError(t, err)
+	defer conn.Close()
+
+	id := bson.NewObjectId()
+
+	user := &models.User{
+		Id:       id,
+		Name:     "Test",
+		Email:    fmt.Sprintf("%s@email.test", id.Hex()),
+		Password: "123456789",
+		Created:  time.Now(),
+		Updated:  time.Now(),
+	}
+
+	r := NewUsersRepository(conn)
+	err = r.Save(user)
+	assert.NoError(t, err)
+
+	found, err := r.GetById(user.Id.Hex())
+	assert.NoError(t, err)
+	assert.Equal(t, user.Name, found.Name)
+	assert.Equal(t, user.Id, found.Id)
+	assert.Equal(t, user.Email, found.Email)
+	assert.Equal(t, user.Password, found.Password)
+}
--- go.mod
+++ go.mod
@@ -5,6 +5,8 @@ go 1.16
 require (
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/joho/godotenv v1.3.0
+	github.com/stretchr/testify v1.7.0
+	google.golang.org/grpc v1.39.0 // indirect
 	google.golang.org/protobuf v1.27.1 // indirect
 	gopkg.in/mgo.v2 v2.0.0-20190816093944-a6b53ec6cb22
 )
