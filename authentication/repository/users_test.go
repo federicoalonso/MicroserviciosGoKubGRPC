68c14103e9374fb8f476f1ad1ed846c551a98178
Federico Alonso <f.nicolas.alonso@gmail.com>
2021-07-12 21:52:10 -0300

    listo pruebas de usuario

--- authentication/repository/users_test.go
+++ authentication/repository/users_test.go
@@ -10,6 +10,7 @@ import (
 
 	"github.com/joho/godotenv"
 	"github.com/stretchr/testify/assert"
+	"gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
 )
 
@@ -43,8 +44,141 @@ func TestUsersRepositorySave(t *testing.T) {
 
 	found, err := r.GetById(user.Id.Hex())
 	assert.NoError(t, err)
+	assert.NotNil(t, found)
+}
+
+func TestUsersRepositoryGetById(t *testing.T) {
+	cfg := db.NewConfig()
+	conn, err := db.NewConnection(cfg)
+	assert.NoError(t, err)
+	defer conn.Close()
+
+	id := bson.NewObjectId()
+
+	user := &models.User{
+		Id:       id,
+		Name:     "Test",
+		Email:    fmt.Sprintf("%s@email.test", id.Hex()),
+		Password: "123456789",
+		Created:  time.Now(),
+		Updated:  time.Now(),
+	}
+
+	r := NewUsersRepository(conn)
+	err = r.Save(user)
+	assert.NoError(t, err)
+
+	found, err := r.GetById(user.Id.Hex())
+	assert.NoError(t, err)
 	assert.Equal(t, user.Name, found.Name)
 	assert.Equal(t, user.Id, found.Id)
 	assert.Equal(t, user.Email, found.Email)
 	assert.Equal(t, user.Password, found.Password)
+
+	found, err = r.GetById(bson.NewObjectId().Hex())
+	assert.Error(t, err)
+	assert.EqualError(t, mgo.ErrNotFound, err.Error())
+	assert.Nil(t, found)
+}
+
+func TestUsersRepositoryGetByEmail(t *testing.T) {
+	cfg := db.NewConfig()
+	conn, err := db.NewConnection(cfg)
+	assert.NoError(t, err)
+	defer conn.Close()
+
+	id := bson.NewObjectId()
+
+	user := &models.User{
+		Id:       id,
+		Name:     "Test",
+		Email:    fmt.Sprintf("%s@email.test", id.Hex()),
+		Password: "123456789",
+		Created:  time.Now(),
+		Updated:  time.Now(),
+	}
+
+	r := NewUsersRepository(conn)
+	err = r.Save(user)
+	assert.NoError(t, err)
+
+	found, err := r.GetByEmail(user.Email)
+	assert.NoError(t, err)
+	assert.Equal(t, user.Name, found.Name)
+	assert.Equal(t, user.Id, found.Id)
+	assert.Equal(t, user.Email, found.Email)
+	assert.Equal(t, user.Password, found.Password)
+
+	found, err = r.GetByEmail("")
+	assert.Error(t, err)
+	assert.EqualError(t, mgo.ErrNotFound, err.Error())
+	assert.Nil(t, found)
+}
+
+func TestUsersRepositoryUpdate(t *testing.T) {
+	cfg := db.NewConfig()
+	conn, err := db.NewConnection(cfg)
+	assert.NoError(t, err)
+	defer conn.Close()
+
+	id := bson.NewObjectId()
+
+	user := &models.User{
+		Id:       id,
+		Name:     "Test",
+		Email:    fmt.Sprintf("%s@email.test", id.Hex()),
+		Password: "123456789",
+		Created:  time.Now(),
+		Updated:  time.Now(),
+	}
+
+	r := NewUsersRepository(conn)
+	err = r.Save(user)
+	assert.NoError(t, err)
+
+	found, err := r.GetByEmail(user.Email)
+	assert.NoError(t, err)
+	assert.NotNil(t, found)
+
+	user.Name = "Update test"
+	err = r.Update(user)
+	assert.NoError(t, err)
+
+	found, err = r.GetByEmail(user.Email)
+	assert.NoError(t, err)
+	assert.Equal(t, "Update test", found.Name)
+}
+
+func TestUsersRepositoryDelete(t *testing.T) {
+	cfg := db.NewConfig()
+	conn, err := db.NewConnection(cfg)
+	assert.NoError(t, err)
+	defer conn.Close()
+
+	id := bson.NewObjectId()
+
+	user := &models.User{
+		Id:       id,
+		Name:     "Test",
+		Email:    fmt.Sprintf("%s@email.test", id.Hex()),
+		Password: "123456789",
+		Created:  time.Now(),
+		Updated:  time.Now(),
+	}
+
+	r := NewUsersRepository(conn)
+	err = r.Save(user)
+	assert.NoError(t, err)
+
+	found, err := r.GetByEmail(user.Email)
+	assert.NoError(t, err)
+	assert.NotNil(t, found)
+
+	err = r.Delete(user.Id.Hex())
+	assert.NoError(t, err)
+
+	found, err = r.GetByEmail(user.Email)
+	assert.Error(t, err)
+	assert.EqualError(t, mgo.ErrNotFound, err.Error())
+	assert.Nil(t, found)
 }
