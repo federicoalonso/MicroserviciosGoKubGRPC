d3c82f38f83262e76a85595caab07d45115580e0
Federico Alonso <f.nicolas.alonso@gmail.com>
2021-07-14 00:00:36 -0300

    servicio levantado

--- authentication/main.go
+++ authentication/main.go
@@ -2,18 +2,26 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
+	"microservicios/authentication/repository"
+	"microservicios/authentication/service"
 	"microservicios/db"
+	"microservicios/pb"
+	"net"
 
 	"github.com/joho/godotenv"
+	"google.golang.org/grpc"
 )
 
 var (
 	local bool
+	port  int
 )
 
 func init() {
-	flag.BoolVar(&local, "local", true, "run service local")
+	flag.IntVar(&port, "port", 9015, "authentication service port")
+	flag.BoolVar(&local, "local", true, "run authentication service local")
 	flag.Parse()
 }
 
@@ -31,4 +39,17 @@ func main() {
 		log.Panic(err)
 	}
 	defer conn.Close()
+
+	usersRepository := repository.NewUsersRepository(conn)
+	authService := service.NewAuthService(usersRepository)
+
+	lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", port))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	grpcServer := grpc.NewServer()
+	pb.RegisterAuthServiceServer(grpcServer, authService)
+
+	log.Printf("Authentication service running on [::]:%d\n", port)
+	grpcServer.Serve(lis)
 }
